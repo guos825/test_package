5eed8facaa87baf7ee4bc8361a71ebfac78f3050
guoshuai825 <448139528@qq.com>
2020-01-02 11:42:53 +0800

    init

---
+++ pipenvtest/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+setup(
+    name='test',
+    version=open('version', 'r').read().strip(),
+    packages=['guoshuai'],
+    url='https://github.com/guos825/test_package.git',
+    license='MIT',
+    author='Guo Shuai',
+    author_email='448139528@qq.com',
+    description='GS Test SDK for Python',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), "README.md"), 'r').read(),
+    install_requires=[]
+)
---
+++ pipenvtest/test.py
@@ -0,0 +1,9 @@
+
+class Test:
+
+    def __int__(self):
+        print("__int__")
+
+    def test(self):
+        print("test")
+
